04699f4f8516ba78929527b13ccbf4059bb501a8
mpatel <mpatel@QAT30.IN.CAPITALNOVUS.COM>
2017-09-14 16:58:53 +0530

    hello

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+
+public class HelloWorld {
+
+	private static Integer integer;
+
+public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Test");
+	
+
+}
